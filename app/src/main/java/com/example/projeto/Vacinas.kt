f8a23d0d387a5426bb181cef6986885571d0a7f2
IanAquino <ianpedroaquino@gmail.com>
2021-05-20 19:52:16 +0100

    tabela vacinas

--- app/src/main/java/com/example/projeto/Vacinas.kt
+++ app/src/main/java/com/example/projeto/Vacinas.kt
@@ -1,4 +1,31 @@
 package com.example.projeto
 
-class Vacinas {
+import android.content.ContentValues
+
+data class Vacinas(var id: Long = -1, var nome: String) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+        valores.put(TabelaVacinas.CAMPO_NOME, nome)
+
+        return valores
+    }
+    companion object{
+        fun fromCursor(): Pacientes{
+            val colId = cursor.getColumnIndex(BaseColumns._ID)
+            val colNome = cursor.getColumnIndex(TabelaVacinas.NOME_TABELA)
+            val colEntrada = cursor.getColumnIndex(TabelaVacinas.ENTRADA_VACINAS)
+            val colSaida = cursor.getColumnIndex(TabelaVacinas.SAIDA_VACINAS)
+            val colCategoria = cursor.getColumnIndex(TabelaVacinas.CATEGORIA)
+
+            val id = cursor.getLong(0)
+            val nome = cursor.getString(1)
+            val entrada = cursor.getInteger(1)
+            val saida = cursor.getInteger(1)
+            val categoria = cursor.getString(1)
+
+
+            return Vacinas(id, nome, entrada, saida, categoria)
+        }
+
+    }
 }
