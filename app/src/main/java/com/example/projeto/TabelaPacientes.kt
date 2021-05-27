85168564a101d0f456c4ef99696ca4179c382364
IanAquino <ianpedroaquino@gmail.com>
2021-05-27 13:37:15 +0100

    Melhoria tabela Pacientes

--- app/src/main/java/com/example/projeto/TabelaPacientes.kt
+++ app/src/main/java/com/example/projeto/TabelaPacientes.kt
@@ -11,7 +11,7 @@ class TabelaPacientes(db: SQLiteDatabase)  {
     private val db: SQLiteDatabase = db
 
         fun cria() {
-            db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $DATA_NASCIMENTO TEXT NOT NULL, $CAMPO_CONTACTO TEXT NOT NULL)")
+            db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $DATA_NASCIMENTO INT NOT NULL, $CAMPO_CONTACTO TEXT NOT NULL, , $MORADA TEXT NOT NULL, , $NIF TEXT NOT NULL, , $ESTADO TEXT NOT NULL)")
         }
 
         fun insert(values: ContentValues): Long {
@@ -34,7 +34,8 @@ class TabelaPacientes(db: SQLiteDatabase)  {
             having: String?,
             orderBy: String?
         ): Cursor? {
-            return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+            return db.query(NOME_TABELA, columns,
+                selection.toString(), selectionArgs, groupBy, having, orderBy)
         }
 
         companion object {
@@ -42,6 +43,10 @@ class TabelaPacientes(db: SQLiteDatabase)  {
             const val CAMPO_NOME = "nome"
             const val DATA_NASCIMENTO = "nascimento"
             const val CAMPO_CONTACTO = "contacto"
+            const val MORADA = "morada"
+            const val NIF = "nif"
+            const val ESTADO = "estado"
+
 
         }
     }
